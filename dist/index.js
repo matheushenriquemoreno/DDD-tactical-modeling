fd91f7d573fec6cf77fe4a1b89ce8c99950d7cd5
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2024-08-31 15:28:50 -0300

    fix: removendo pasta de publicacao

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = hello;
-function hello(name) {
-    console.log("Hello");
-    return `hello ${name}`;
-}
-hello("matheus");
